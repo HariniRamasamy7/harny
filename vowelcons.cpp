3dc61816af94cb783965312059630607aab916f9
HariniRamasamy7 <36732343+HariniRamasamy7@users.noreply.github.com>
2018-02-23 18:03:02 +0530

    vowelscon.cpp

---
+++ vowelcons.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{ char vow;
+  cout<<"enter the character";
+  cin>>vow;
+  if(vow=='a'||vow=='e'||vow='i'||vow='o'||vow=='u'||vow=='A'||vow='E'||vow='I'|| vow='O'|| vow='U')
+  { cout<<"given character is a vowel";}
+  else{ 
+  	cout<<"given character is a consonent";
+  	
+  }
+  return 0;
+  }
