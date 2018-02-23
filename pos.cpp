4f94b12b81fb85b6692b8f5a901671917dd0dec3
HariniRamasamy7 <36732343+HariniRamasamy7@users.noreply.github.com>
2018-02-23 17:22:25 +0530

    pos.cpp

---
+++ pos.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+  int n;
+  cout<<"enter n";
+  cin>>n;
+  if(n==0)
+  { cout<<"given number is zero";
+  }
+  else {if(n>0)
+  { cout<<"given number is positive";
+  } 
+  else
+  { cout<<"given number is negative";
+  }
+  }
+  return 0;
+  }
+  
