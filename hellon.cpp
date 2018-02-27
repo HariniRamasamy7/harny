c3501447b444b89c1d60f58691eb2a27abc490ce
HariniRamasamy7 <36732343+HariniRamasamy7@users.noreply.github.com>
2018-02-27 17:43:02 +0530

    hellon.cpp

---
+++ hellon.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{ char c="hello";
+  int n;
+  cout<<"enter n";
+  cin>>n;
+  for( int i=1;i<=n;i++)
+  { cout<< c<<endl;
+    }
+  return 0;
+  }
+  
