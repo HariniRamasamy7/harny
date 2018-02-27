e5c765e550701668cb5e22db201514f0f8299503
HariniRamasamy7 <36732343+HariniRamasamy7@users.noreply.github.com>
2018-02-27 17:33:36 +0530

    leap.cpp

---
+++ leap.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int y;
+  cout<<"enter year";
+  cin>>y;
+  if(y%4==0)
+  { cout<<" given year is leap year";}
+  else{ cout<<"given year is not leap year";
+  }return 0;
+  }
