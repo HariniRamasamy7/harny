83c27635d5659778fcb7c2f974ce3db39da87e9a
HariniRamasamy7 <36732343+HariniRamasamy7@users.noreply.github.com>
2018-02-27 17:16:57 +0530

    alpha.cpp

---
+++ alpha.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ char c;
+ cout<<"enter c:";
+ cin>>c;
+ if(c>='a'&&c<='z')
+ { cout<<"given is alphabet";
+ }
+ else { cout<<"given number is not alphabet";}
+ return 0;
+ }
