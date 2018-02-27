fe64c2f6472be44c06f317861d273122fd084357
HariniRamasamy7 <36732343+HariniRamasamy7@users.noreply.github.com>
2018-02-27 18:00:34 +0530

    sumnatr.cpp

---
+++ sumnatr.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int n,s=0;
+  cout<<"enter n";
+  cin>>n;
+  for( int i=0;i<n;i++)
+  {
+  s=i+1;
+  }
+  cout<<"the sum of natural numbers are"<<'\t'<<s;
+  return 0;
+  }
+  
