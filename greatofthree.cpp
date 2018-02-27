a49263ab719e6c18d1b6e35ea7793bd56ad62c22
HariniRamasamy7 <36732343+HariniRamasamy7@users.noreply.github.com>
2018-02-27 17:25:24 +0530

    greatofthree.cpp

---
+++ greatofthree.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int a,b,c;
+ cout<<"enter a,b,c";
+ cin>>a>>b>>c;
+ if(a>b)
+ {cout<<"a is greater";
+ }
+ else if(b>c)
+ {cout<<"b is greater";
+ } 
+ else { cout<<"c is greater";}
+ return 0;
+ }
