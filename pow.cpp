1b88c209248d58240f56718cefbe4cfd81602f9b
HariniRamasamy7 <36732343+HariniRamasamy7@users.noreply.github.com>
2018-02-28 17:23:15 +0530

    pow.cpp

---
+++ pow.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main ()
+{
+int num1,num2,power;
+cout<<"enter num1 and num2";
+cin>>num1>>num2;
+power=pow(num1,num2);
+cout<<"the power of numbers are"<<power;
+return 0;
+}
