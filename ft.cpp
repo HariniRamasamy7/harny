b5e8f0471441c06942b057d3055740c9c142dce7
HariniRamasamy7 <36732343+HariniRamasamy7@users.noreply.github.com>
2018-02-28 19:55:32 +0530

    ft.cpp

---
+++ ft.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num,fact=1;
+cout<<"enter positive integer";
+cin>>num;
+if(num<=20)
+{
+for(int i=1;i<=n;i++)
+{
+fact=fact*i;
+}
+cout<<"the factorial is"<<fact;
+}
+else
+cout<<"invalid input";
+return 0;
+}
+
