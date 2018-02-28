439561a7b132fdc82ab97ee3a62df19e4d987587
HariniRamasamy7 <36732343+HariniRamasamy7@users.noreply.github.com>
2018-02-28 17:13:02 +0530

    sumofk.cpp

---
+++ sumofk.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num,a[num],k,s;
+cout<<"enter the value of num";
+cin>>num;
+cout<<"enter k";
+cin>>k;
+for(int i=0;i<=n;i++)
+{
+cin>>a[i];
+}
+s=a[0]+k;
+cout<<"ans is"<<s;
+return 0;
+}
