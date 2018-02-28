84db90e55c1f5d9d8d0d0303a78c06832e486a0d
HariniRamasamy7 <36732343+HariniRamasamy7@users.noreply.github.com>
2018-02-28 17:47:26 +0530

    spaceeven.cpp

---
+++ spaceeven.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num1,num2;
+cout<<"enter value of num1 and num2";
+cin>>num1>>num2;
+for(int i=num1;i<num2;i++)
+{
+if(i%2==0)
+{
+cout<<i<<endl;
+}
+}
+return 0;
+}
