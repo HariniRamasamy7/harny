3324c258d6d839186d5f09d104f94ce5d0a198ec
HariniRamasamy7 <36732343+HariniRamasamy7@users.noreply.github.com>
2018-03-01 17:14:06 +0530

    append.cpp

---
+++ append.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{ string s;
+  cout<<"enter the string";
+  cin>>s;
+  s.push_back('.');
+  cout<<"the appended string is"<<s;
+  return 0;
+  }
