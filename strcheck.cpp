d63a7f1e37aeb3054eafd4cc1ba2c94b228fbaad
HariniRamasamy7 <36732343+HariniRamasamy7@users.noreply.github.com>
2018-03-02 18:03:20 +0530

    strcheck.cpp

---
+++ strcheck.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{ char* str1,str2;
+  cout<<"enter str";
+  cin>>str1>>str2;
+  if(strlen(str1==str2)
+  { cout<<"no";
+  }
+  else{ cout<<"yes";
+  }
+  return 0;
+  }
