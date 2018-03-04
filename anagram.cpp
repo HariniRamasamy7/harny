d6c3c1638d987539c536670a273926af89254445
HariniRamasamy7 <36732343+HariniRamasamy7@users.noreply.github.com>
2018-03-04 09:54:27 -0800

    anagram.cpp

---
+++ anagram.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{ int i,count=0,len,len2;
+string str1,str2;
+cin>>str1;
+for(i=0;i<=3;i++)
+{ cin>>str2[i];
+}
+for(int j=0;j<=3;j++)
+{count=count+str2[i];}
+len=str1.length();
+len2=str2.length();
+if(len==len2)
+{ 
+
+  cout<<"the count is"<<count;
+  return 0;
+  }
+  
+  }
